6fffb008b14f4c5aebe749b6596a5987606f8c35
cgerman <abstfact87@gmail.com>
2020-01-05 14:49:51 -0600

    made some changes

--- MoneyCalculatorTests/CalculatorTests.cs
+++ MoneyCalculatorTests/CalculatorTests.cs
@@ -1,19 +1,19 @@
-﻿
-using WindowsFormsApp2;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using NUnit.Framework.Internal;
+using RegisterDropCalculator;
 
 namespace MoneyCalculatorTests
 {
     [TestFixture]
     public class CalculatorTests
     {
-        private double _registerTotal = 0;//amount register is supposed to be at for drop
+        private double _registerTotal = 0; //amount register is supposed to be at for drop
+
         [Test]
         public void TestAllZeros()
         {
             _registerTotal = 0;
-            var moneyAmounts = TestHelper.BuildMoneyAmounts(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,_registerTotal);
+            var moneyAmounts = TestHelper.BuildMoneyAmounts(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, _registerTotal);
             var sut = new Calculator(moneyAmounts);
             var expected = _registerTotal;
             var actual = sut.GetDropTotal();
@@ -24,7 +24,7 @@ namespace MoneyCalculatorTests
         public void TestAllOnes()
         {
             _registerTotal = 187.91;
-            var moneyAmounts = TestHelper.BuildMoneyAmounts(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,_registerTotal);
+            var moneyAmounts = TestHelper.BuildMoneyAmounts(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, _registerTotal);
             var sut = new Calculator(moneyAmounts);
             var expected = _registerTotal;
             var actual = sut.GetDropTotal();
